41515fc90fa4cdf701bd0763cef984c70201781a
Andrew K <nscapital08@gmail.com>
2023-01-08 01:00:08 +0300

    feat: added ride stats to stats page

--- src/Components/Stats/Stats.tsx
+++ src/Components/Stats/Stats.tsx
@@ -3,14 +3,20 @@ import { useNavigate } from 'react-router-dom';
 import { getActivities, getCurrentAthlete, getAthlete } from '../../utils/stravaApi';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import { Profile } from '../../models/Profile';
+import { AthleteStats } from '../../models/AthleteStats';
 
 export default function Stats() {
 
   const currentUser = React.useContext<Profile>(CurrentUserContext);
 
-  const [allRidesTotals, setAllRidesTotals] = useState<string>('')
+  const [allRidesTotals, setAllRidesTotals] = useState<AthleteStats>({} as AthleteStats);
+  const [allYTDRidesTotals, setAllYTDRidesTotals] = useState<AthleteStats>({} as AthleteStats)
   console.log(allRidesTotals);
+  console.log(currentUser)
 
+
+  const allRidesDistance: number = allRidesTotals.distance / 1000;
+  const yTDRidesDistance: number = allYTDRidesTotals.distance / 1000;
   // function getTrainings() {
   //   getActivities()
   //     .then((res) => console.log(res))
@@ -23,27 +29,32 @@ export default function Stats() {
   //     .catch((err) => console.log(err));
   // }
 
-  function getUser() {
-    getAthlete(currentUser.id)
-      .then((res) => setAllRidesTotals(res.all_ride_totals))
+  function getUserStats(user: Profile) {
+    getAthlete(user.id)
+      .then((res) => {
+        setAllRidesTotals((res.all_ride_totals));
+        setAllYTDRidesTotals(res.ytd_ride_totals);
+      })
       .catch((err) => console.log(err));
   }
 
   useEffect(() => {
-    getUser();
-  }, []);
+    if(currentUser.id) {
+      getUserStats(currentUser)
+    };
+  }, [currentUser]);
 
 
 
 
   return (
-    <section>
+    <section className='stats'>
       {/* <button type='button' onClick={getTrainings}>Get trainings</button>
       <button type='button' onClick={getUser}>Get user</button> */}
-      <p>Количество тренировок за всё время:</p>
-      <p>Пройдено км за всё время: {allRidesTotals}</p>
-      <p>Количество тренировок за год:</p>
-      <p>Пройдено км за год:</p>
+      <p>Количество тренировок за всё время: <span className='stats__text_bold'>{allRidesTotals.count}</span></p>
+      <p>Пройдено км за всё время: <span className='stats__text_bold'>{allRidesDistance ? Math.floor(allRidesDistance) : ''}</span> км</p>
+      <p>Количество тренировок за год: <span className='stats__text_bold'>{allYTDRidesTotals.count}</span></p>
+      <p>Пройдено км за год: <span className='stats__text_bold'>{yTDRidesDistance ? Math.floor(yTDRidesDistance) : ''}</span> км</p>
     </section>
   )
 }
---
+++ src/models/AthleteStats.ts
@@ -0,0 +1,7 @@
+export interface AthleteStats {
+  count: number
+  distance: number
+  elapsed_time: number
+  elevation_gain: number
+  moving_time: number
+}
