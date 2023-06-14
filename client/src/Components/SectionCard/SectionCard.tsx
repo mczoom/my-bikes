a6b15051805dde1989c0f669886a8ed446e1f085
unknown <nscapital08@gmail.com>
2023-06-15 01:20:38 +0300

    style: changed cards design on Main page

--- client/src/Components/SectionCard/SectionCard.tsx
+++ client/src/Components/SectionCard/SectionCard.tsx
@@ -10,22 +10,18 @@ interface SectionCardProps {
 
 export default function SectionCard({card}: SectionCardProps) {
   return (
-    
-    <Link to={card.path} className='section-card link' >
-      <div className='section-card__container'>
-        <div className='section-card__wrapper'>        
-          <img src={card.icon} className='section-card__image' alt='иконка раздела'></img>
-          <h2 className='section-card__header'>{card.title}</h2>
-        </div>
-        <figure className='section-card__content'>
-          <img src={card.cover} className='section-card__cover'></img> 
-          <figcaption className='section-card__description'>
-            <h3 className='description__header'>{card.header}</h3>        
-            <p className='description__text'>{card.text}</p>
-          </figcaption>
-        </figure>
-      </div>
-    </Link>
-    
+    <Link to={card.path} className='link'>
+    <div className='section-card'>        
+      <figure className='section-card__content'>          
+        <img src={card.cover} className='section-card__cover'></img> 
+        <div className='section-card__gradient'></div>        
+        <h2 className='section-card__header'>{card.title}</h2>        
+        <figcaption className='section-card__description'>
+          <h3 className='description__header'>{card.header}</h3>        
+          <p className='description__text'>{card.text}</p>
+        </figcaption>
+      </figure>
+    </div> 
+   </Link>
   )
 }
